8b8b9d16554d568051a3b36a5cbf293edaf2a07d
Emmanuel Isenah <emmanuelisenah@gmail.com>
2023-11-18 22:32:34 +0100

    refactor: update image upload hook

--- src/lib/s3.ts
+++ src/lib/s3.ts
@@ -1,46 +1,25 @@
-import { Sha256 } from "@aws-crypto/sha256-browser";
-import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
-import {
-  S3Client,
-  PutObjectCommand,
-  PutObjectCommandInput,
-  GetObjectCommand,
-} from "@aws-sdk/client-s3";
-import { nanoid } from "nanoid";
+import { getSignedURL } from "@/actions/upload/getSignedURL";
+import axios from "axios";
 
-const bucketName = process.env.NEXT_PUBLIC_AWS_BUCKET_NAME;
-const region = process.env.NEXT_PUBLIC_AWS_BUCKET_REGION;
-const accessKeyId = process.env.NEXT_PUBLIC_AWS_ACCESS_KEY;
-const secretAccessKey = process.env.NEXT_PUBLIC_AWS_SECRET_ACCESS_KEY;
+export const uploadImageToS3 = async (file: File): Promise<string | null> => {
+  const res = await getSignedURL({ fileType: file.type, fileSize: file.size });
+  if (res.failure !== undefined) {
+    console.error(res.failure);
+    return res.failure;
+  }
 
-const s3Client = new S3Client({
-  region: region,
-  credentials: {
-    accessKeyId: accessKeyId ?? "",
-    secretAccessKey: secretAccessKey ?? "",
-  },
-  sha256: Sha256,
-});
-
-export const uploadImageToS3 = async (file: File): Promise<string> => {
-  // upload details to send to S3
-  const params: PutObjectCommandInput = {
-    Bucket: bucketName ?? "",
-    Key: `${nanoid()}-${file.name}`,
-    Body: file,
-    ContentType: file.type,
-  };
+  const { url } = res.success;
   try {
-    const command = new PutObjectCommand(params);
-    await s3Client.send(command);
-    const imageUrl = await getSignedUrl(s3Client, new GetObjectCommand({
-      Bucket: bucketName ?? "",
-      Key: params.Key ?? "",
-    }), {
-      expiresIn: 3600,
+    await axios.put(url, file, {
+      headers: {
+        "Content-Type": file.type,
+      },
     });
-    return imageUrl;
+
+    console.log("File uploaded to S3", url);
+    return url.split("?")[0];
   } catch (error) {
-    throw error;
+    console.error("Error uploading file to S3", error);
+    return null;
   }
 };
