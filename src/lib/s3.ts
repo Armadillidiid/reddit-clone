968f963775e83c873e79288ad032f3cc5aa2b33c
Emmanuel Isenah <emmanuelisenah@gmail.com>
2023-11-18 19:15:00 +0100

    fix: resolve SHA signing

--- src/lib/s3.ts
+++ src/lib/s3.ts
@@ -1,15 +1,17 @@
+import { Sha256 } from "@aws-crypto/sha256-browser";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import {
   S3Client,
   PutObjectCommand,
   PutObjectCommandInput,
+  GetObjectCommand,
 } from "@aws-sdk/client-s3";
 import { nanoid } from "nanoid";
 
-const bucketName = process.env.AWS_BUCKET_NAME;
-const region = process.env.AWS_BUCKET_REGION;
-const accessKeyId = process.env.AWS_ACCESS_KEY;
-const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
+const bucketName = process.env.NEXT_PUBLIC_AWS_BUCKET_NAME;
+const region = process.env.NEXT_PUBLIC_AWS_BUCKET_REGION;
+const accessKeyId = process.env.NEXT_PUBLIC_AWS_ACCESS_KEY;
+const secretAccessKey = process.env.NEXT_PUBLIC_AWS_SECRET_ACCESS_KEY;
 
 const s3Client = new S3Client({
   region: region,
@@ -17,6 +19,7 @@ const s3Client = new S3Client({
     accessKeyId: accessKeyId ?? "",
     secretAccessKey: secretAccessKey ?? "",
   },
+  sha256: Sha256,
 });
 
 export const uploadImageToS3 = async (file: File): Promise<string> => {
@@ -27,17 +30,17 @@ export const uploadImageToS3 = async (file: File): Promise<string> => {
     Body: file,
     ContentType: file.type,
   };
-
   try {
     const command = new PutObjectCommand(params);
     await s3Client.send(command);
-    const imageUrl = await getSignedUrl(s3Client, command, {
+    const imageUrl = await getSignedUrl(s3Client, new GetObjectCommand({
+      Bucket: bucketName ?? "",
+      Key: params.Key ?? "",
+    }), {
       expiresIn: 3600,
     });
-    console.log("Image uploaded successfully:", imageUrl);
     return imageUrl;
   } catch (error) {
-    console.error("Error uploading file to S3:", error);
     throw error;
   }
 };
