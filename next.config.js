f1c2e919b362fbd2202a30e13ad8e4e7ebb420e6
Emmanuel Isenah <emmanuelisenah@gmail.com>
2023-11-26 16:23:38 +0100

    refactor: update next config to use remotePatterns

--- next.config.js
+++ next.config.js
@@ -1,7 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['uploadthing.com', 'lh3.googleusercontent.com'],
+    remotePatterns: [
+      // {
+      //   protocol: 'https',
+      //   hostname: 'uploadthing.com',
+      //   port: '',
+      //   pathname: '/**',
+      // },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: "reddit-clone-24gxagmphhhw3x.s3.eu-north-1.amazonaws.com",
+        port: '',
+        pathname: '/**',
+      }
+    ],
   },
 }
 
