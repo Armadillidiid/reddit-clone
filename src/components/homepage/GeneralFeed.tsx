a04472512d9196448759da34d3d6945645009164
Emmanuel Isenah <emmanuelisenah@gmail.com>
2023-12-10 10:04:08 +0100

    feat: add CustomFeed ad GeneralFeed component to homepage

---
+++ src/components/homepage/GeneralFeed.tsx
@@ -0,0 +1,24 @@
+import { db } from '@/lib/db'
+import PostFeed from '../PostFeed'
+import { INFINITE_SCROLL_PAGINATION_RESULTS } from '@/config'
+import { getAuthSession } from '@/lib/auth'
+
+const GeneralFeed = async () => {
+  const session = await getAuthSession()
+  const posts = await db.post.findMany({
+    orderBy: {
+      createdAt: 'desc',
+    },
+    include: {
+      votes: true,
+      author: true,
+      comments: true,
+      subreddit: true,
+    },
+    take: INFINITE_SCROLL_PAGINATION_RESULTS, // 4 to demonstrate infinite scroll, should be higher in production
+  })
+
+  return <PostFeed initialPosts={posts} session={session} />
+}
+
+export default GeneralFeed
