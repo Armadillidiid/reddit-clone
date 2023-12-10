5a44718d885fd8ea665eb47bf93831f8d46b903a
Emmanuel Isenah <emmanuelisenah@gmail.com>
2023-12-10 09:15:39 +0100

    feat: add react-query dev tools

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,21 +1,23 @@
-'use client'
+"use client";
 
-import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
-import { SessionProvider } from 'next-auth/react'
-import { FC, ReactNode } from 'react'
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { SessionProvider } from "next-auth/react";
+import { FC, ReactNode } from "react";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 interface LayoutProps {
-  children: ReactNode
+  children: ReactNode;
 }
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
 const Providers: FC<LayoutProps> = ({ children }) => {
   return (
     <QueryClientProvider client={queryClient}>
       <SessionProvider>{children}</SessionProvider>
+      <ReactQueryDevtools />
     </QueryClientProvider>
-  )
-}
+  );
+};
 
-export default Providers
+export default Providers;
